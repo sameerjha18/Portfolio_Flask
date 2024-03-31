6cdb000f5cbd4ff1b9ccb1040d5b78ec10e57a0d
sameer jha <sameerranjan999@gmail.com>
2024-03-31 17:40:27 +0530

    change the port

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,13 @@
 from flask import Flask
 from .blueprints.main import main_bp
 
+
 def create_app():
     app = Flask(__name__)
 
     # Register blueprints
     app.register_blueprint(main_bp)
-    
+
     # More blueprints can be registered here
 
     return app
